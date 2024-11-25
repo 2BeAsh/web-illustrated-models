23405daf683dd2aa866bb7ad12c3b9c9df2cb963
2BeAsh <109065992+2BeAsh@users.noreply.github.com>
2024-11-25 11:20:59 +0100

    Update quadratic_streamlit.py

--- jupyter/quadratic_streamlit.py
+++ jupyter/quadratic_streamlit.py
@@ -9,7 +9,7 @@ st.sidebar.title('Sinusoidal Animation')
 frequency = st.sidebar.slider("Frequency", min_value=0.1, max_value=5.0, value=1.0, step=0.1)
 
 # Create frames for sine wave
-x = np.linspace(0, 4 * np.pi, 10_000)
+x = np.linspace(0, 4 * np.pi, 1000)
 frames = []
 
 for i in range(200):
@@ -25,10 +25,12 @@ fig = go.Figure(
     frames=frames,
     layout=go.Layout(
         updatemenus=[
-            dict(type='buttons', showactive=False,
-                 buttons=[dict(label='Play',
-                 method="animate",
-                 args=[None]
+            dict(type='buttons', 
+                 showactive=False,
+                 buttons=[
+                     dict(label='Play',
+                          method="animate",
+                          args=[None, {"mode": "immediate", "frame" : {"redraw": True}, "fromcurrent": True}],
                  )])]))
 
 
