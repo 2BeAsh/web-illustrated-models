bc34b1bc4e85cb7e3c58786b5a2785175e468399
2BeAsh <109065992+2BeAsh@users.noreply.github.com>
2024-11-27 09:53:44 +0100

    temp remove sandpile

--- jupyter/main.py
+++ jupyter/main.py
@@ -4,11 +4,10 @@ import streamlit as st
 from ising_model import IsingModel
 from sine_wave_model import SineWaveModel
 from network import ErdosRenyiNetworkModel
-# from sandpile_model import SandpileModel
 
 # Model selection
 st.sidebar.header("Select Model")  
-model = st.sidebar.selectbox(label="Choose a model", options=["Sine wave", "Ising", "Erdos-Renyi Network",])
+model = st.sidebar.selectbox(label="", options=["Sine wave", "Ising", "Erdos-Renyi Network"])
 
 
 if model == "Ising":
@@ -22,7 +21,3 @@ elif model == "Sine wave":
 elif model == "Erdos-Renyi Network":
     network_model = ErdosRenyiNetworkModel()
     network_model.animate()
-    
-# elif model == "Sandpile":
-#     sandpile_model = SandpileModel()
-#     sandpile_model.animate()
